cda41d2735ccd79875db1e60c74dadabc44e1ee3
Jennifer Marrocco <jlmarroc@myseneca.ca>
2025-05-29 22:54:19 -0400

    Step 4: Added explicit return to hello()

--- functions_part1.py
+++ functions_part1.py
@@ -1,7 +1,8 @@
 def hello():
     print('Hello World')
     print('Inside a Function')
+    return 
 
-hello()
-hello()
-hello()
+my_stuff = hello()
+print('Stuff return from hello():', my_stuff)
+print('The object my_stuff is a type:', type(my_stuff))
